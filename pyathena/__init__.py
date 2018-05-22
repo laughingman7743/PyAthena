cca5738933f1c367ba14e6af63af7f3f1e02fb5a
laughingman7743 <laughingman7743@gmail.com>
2018-05-22 15:36:38 +0900

    Bump version to 1.2.4

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -6,7 +6,7 @@ import datetime
 
 from pyathena.error import *  # noqa
 
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
 apilevel = '2.0'
