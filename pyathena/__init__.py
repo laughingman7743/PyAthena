a2a6660e5758e5830f8ef49b3b49fe93f4a2d012
laughingman7743 <laughingman7743@gmail.com>
2019-04-14 15:49:29 +0900

    Bump version to 1.6.0

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -6,7 +6,7 @@ import datetime
 
 from pyathena.error import *  # noqa
 
-__version__ = '1.5.1'
+__version__ = '1.6.0'
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
 apilevel = '2.0'
