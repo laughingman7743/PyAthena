86aad2845c477b6e15767129250b553c817dc113
laughingman7743 <laughingman7743@gmail.com>
2019-06-16 18:29:00 +0900

    Bump version to 1.7.0

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -6,7 +6,7 @@ import datetime
 
 from pyathena.error import *  # noqa
 
-__version__ = '1.6.1'
+__version__ = '1.7.0'
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
 apilevel = '2.0'
