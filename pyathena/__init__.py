d20213533387d473386e32d49bba1cf8616ed253
laughingman7743 <laughingman7743@gmail.com>
2019-10-14 08:04:17 +0900

    Bump version to 1.8.0

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -6,7 +6,7 @@ import datetime
 
 from pyathena.error import *  # noqa
 
-__version__ = '1.7.2'
+__version__ = '1.8.0'
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
 apilevel = '2.0'
