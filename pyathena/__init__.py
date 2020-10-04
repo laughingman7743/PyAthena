b50087325a3cc266bfacd7d9ece436a47d49c34c
laughingman7743 <laughingman7743@gmail.com>
2020-10-04 15:10:02 +0900

    Change DBAPITypeObject to frozenset implementation

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -12,38 +12,39 @@ threadsafety: int = 3
 paramstyle: str = "pyformat"
 
 
-class DBAPITypeObject:
+class DBAPITypeObject(frozenset):
     """Type Objects and Constructors
 
     https://www.python.org/dev/peps/pep-0249/#type-objects-and-constructors
     """
 
-    def __init__(self, *values):
-        self.values = values
-
-    def __cmp__(self, other):
-        if other in self.values:
-            return 0
-        if other < self.values:
-            return 1
+    def __eq__(self, other: object):
+        if isinstance(other, frozenset):
+            return frozenset.__eq__(self, other)
         else:
-            return -1
+            return other in self
 
-    def __eq__(self, other):
-        return other in self.values
+    def __ne__(self, other: object):
+        if isinstance(other, frozenset):
+            return frozenset.__ne__(self, other)
+        else:
+            return other not in self
+
+    def __hash__(self):
+        return frozenset.__hash__(self)
 
 
 # https://docs.aws.amazon.com/athena/latest/ug/data-types.html
-STRING: DBAPITypeObject = DBAPITypeObject("char", "varchar", "map", "array", "row")
-BINARY: DBAPITypeObject = DBAPITypeObject("varbinary")
-BOOLEAN: DBAPITypeObject = DBAPITypeObject("boolean")
+STRING: DBAPITypeObject = DBAPITypeObject(("char", "varchar", "map", "array", "row"))
+BINARY: DBAPITypeObject = DBAPITypeObject(("varbinary",))
+BOOLEAN: DBAPITypeObject = DBAPITypeObject(("boolean",))
 NUMBER: DBAPITypeObject = DBAPITypeObject(
-    "tinyint", "smallint", "bigint", "integer", "real", "double", "float", "decimal"
+    ("tinyint", "smallint", "bigint", "integer", "real", "double", "float", "decimal")
 )
-DATE: DBAPITypeObject = DBAPITypeObject("date")
-TIME: DBAPITypeObject = DBAPITypeObject("time", "time with time zone")
-DATETIME: DBAPITypeObject = DBAPITypeObject("timestamp", "timestamp with time zone")
-JSON: DBAPITypeObject = DBAPITypeObject("json")
+DATE: DBAPITypeObject = DBAPITypeObject(("date",))
+TIME: DBAPITypeObject = DBAPITypeObject(("time", "time with time zone"))
+DATETIME: DBAPITypeObject = DBAPITypeObject(("timestamp", "timestamp with time zone"))
+JSON: DBAPITypeObject = DBAPITypeObject(("json",))
 
 Date: Type[datetime.date] = datetime.date
 Time: Type[datetime.time] = datetime.time
