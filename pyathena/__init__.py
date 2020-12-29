76ba9195cd3e8d2060d4fcdbe5deb593767b97d9
laughingman7743 <laughingman7743@gmail.com>
2020-12-29 18:27:57 +0900

    Bump version to 2.0.2

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -7,7 +7,7 @@ from pyathena.error import *  # noqa
 if TYPE_CHECKING:
     from pyathena.connection import Connection
 
-__version__: str = "2.0.1"
+__version__: str = "2.0.2"
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
 apilevel: str = "2.0"
