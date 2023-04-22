40449d8a9750f73492f6dd27b661242d23168cdf
laughingman7743 <laughingman7743@gmail.com>
2023-04-23 00:41:47 +0900

    Bump version to v2.22.1

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -7,7 +7,7 @@ from pyathena.error import *  # noqa
 if TYPE_CHECKING:
     from pyathena.connection import Connection
 
-__version__: str = "2.22.0"
+__version__: str = "2.22.1"
 user_agent_extra: str = f"PyAthena/{__version__}"
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
