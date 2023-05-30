e90c2a49f4d3e7dbebab00f8001e46a0ae9008b7
laughingman7743 <laughingman7743@gmail.com>
2023-05-31 01:20:33 +0900

    Revert "Merge pull request #448 from mdeshmu/dbapi-sqlalchemy-1.4-compat"
    This reverts commit 41f6c2d1edc9171b1e305c602f8855909c9b7e0b, reversing
    changes made to 5ef18dafe93dd708c49d984cc4f43a142bba475d.
    
    # Conflicts:
    #	pyathena/sqlalchemy/base.py

--- pyathena/sqlalchemy/base.py
+++ pyathena/sqlalchemy/base.py
@@ -920,10 +920,6 @@ class AthenaDialect(DefaultDialect):
     def import_dbapi(cls) -> "ModuleType":
         return pyathena
 
-    @classmethod
-    def dbapi(cls) -> "ModuleType":
-        return pyathena
-
     def _raw_connection(self, connection: Union[Engine, "Connection"]) -> "PoolProxiedConnection":
         if isinstance(connection, Engine):
             return connection.raw_connection()
