449e3f426bae0442be7c9afc801204d07f46ff78
laughingman7743 <laughingman7743@gmail.com>
2023-06-29 23:50:51 +0900

    Bump version to v3.0.4

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -9,7 +9,7 @@ from pyathena.error import *  # noqa
 if TYPE_CHECKING:
     from pyathena.connection import Connection
 
-__version__: str = "3.0.3"
+__version__: str = "3.0.4"
 user_agent_extra: str = f"PyAthena/{__version__}"
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
