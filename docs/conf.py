d1c1ea361be7e49b51935e755511e4520aae9e9e
laughingman7743 <laughingman7743@gmail.com>
2024-03-17 19:23:47 +0900

    Apply fmt

--- docs/conf.py
+++ docs/conf.py
@@ -6,24 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PyAthena'
-copyright = '2024, laughingman7743'
-author = 'laughingman7743'
-version = 'v3.4.0'
+project = "PyAthena"
+copyright = "2024, laughingman7743"
+author = "laughingman7743"
+version = "v3.4.0"
 release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.githubpages']
+extensions = ["sphinx.ext.githubpages"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
