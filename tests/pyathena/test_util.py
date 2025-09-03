30ebaa89cd789a5b560390228cc642e9a9fb62f7
Christian Hollinger <christian@ngrok.com>
2025-09-03 11:38:15 -0400

    minor: making retry_if_exception more defensive

--- tests/pyathena/test_util.py
+++ tests/pyathena/test_util.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from typing import Any
+
 import pytest
 
 from pyathena import DataError
-from pyathena.util import parse_output_location, strtobool
+from pyathena.util import RetryConfig, parse_output_location, retry_api_call, strtobool
 
 
 def test_parse_output_location():
@@ -25,3 +27,38 @@ def test_strtobool():
 
     for n in no:
         assert not strtobool(n)
+
+
+class _WithCodeError(Exception):
+    def __init__(self, code: int) -> None:
+        super().__init__(f"error:{code}")
+        self.response = {"Error": {"Code": code}}
+
+
+class _NoResponseError(Exception):
+    def __init__(self) -> None:
+        super().__init__("error")
+        self.response = None
+
+
+def _test_retry(ex: Exception) -> None:
+    calls = {"n": 0}
+
+    def fn() -> Any:
+        calls["n"] += 1
+        raise ex
+
+    cfg = RetryConfig(attempt=1, max_delay=1)
+
+    with pytest.raises(type(ex)):
+        retry_api_call(fn, config=cfg)
+
+    assert calls["n"] == 1
+
+
+def test_retry_api_call():
+    _test_retry(_WithCodeError(500))
+
+
+def test_retry_api_call_with_none_error():
+    _test_retry(_NoResponseError())
